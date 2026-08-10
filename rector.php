bed340eed342ac7a866c8c8d25f6684344ec48ca
Andreas Möller <am@localheinz.com>
2026-08-10 21:43:07 +0200

    Enhancement: Configure ReferenceNamespacedSymbolsRelativeToNamespacePrefixRector

--- rector.php
+++ rector.php
@@ -34,6 +34,13 @@ return static function (Config\RectorConfig $rectorConfig): void {
         Rector\Rules\Faker\GeneratorPropertyFetchToMethodCallRector::class,
     ]);
 
+    $rectorConfig->ruleWithConfiguration(Rector\Rules\Files\ReferenceNamespacedSymbolsRelativeToNamespacePrefixRector::class, [
+        'discoverNamespacePrefixes' => true,
+        'parentNamespacePrefixes' => [
+            'Ergebnis\Json\Normalize',
+        ],
+    ]);
+
     $rectorConfig->sets([
         PHPUnit\Set\PHPUnitSetList::PHPUNIT_100,
     ]);
